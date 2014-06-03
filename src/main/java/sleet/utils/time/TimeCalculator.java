fdab7cc4218945c96e56443ddd0633e8d96cadbc
Matt Mead <matt@formidablecorp.com>
2014-06-03 07:58:44 -0400

    add apache 2.0 license to files

--- src/main/java/sleet/utils/time/TimeCalculator.java
+++ src/main/java/sleet/utils/time/TimeCalculator.java
@@ -1,3 +1,19 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package sleet.utils.time;
 
 import java.util.Calendar;
@@ -7,7 +23,7 @@ import java.util.concurrent.TimeUnit;
 /**
  * Encapsulates the calculation of the time-based portion of sleet ids.
  * 
- * @author mmead
+ * @author matt@matthewcmead.com
  * 
  */
 public class TimeCalculator {
@@ -100,6 +116,9 @@ public class TimeCalculator {
     return this.epoch + (timeValue * this.granularity);
   }
 
+  /**
+   * TODO MCM convert this to a unit test
+   */
   public static void main(String[] args) throws Exception {
     Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
     cal.set(Calendar.YEAR, 2014);
--- src/test/java/sleet/utils/zookeeper/ZkMiniCluster.java
+++ src/test/java/sleet/utils/zookeeper/ZkMiniCluster.java
@@ -1,5 +1,3 @@
-package sleet.utils.zookeeper;
-
 /**
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -16,6 +14,7 @@ package sleet.utils.zookeeper;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package sleet.utils.zookeeper;
 
 import java.io.File;
 import java.io.IOException;
