555640f765b94ad16fcadb3be280dcf7b6593531
Matt Mead <matt@formidablecorp.com>
2014-06-03 07:53:03 -0400

    beef up the testing of sleet by generating a lot of instances to fight over a smallish number of instance slots

--- src/test/java/sleet/TestSleetIdGenerator.java
+++ src/test/java/sleet/TestSleetIdGenerator.java
@@ -3,11 +3,14 @@ package sleet;
 import java.io.File;
 import java.io.InputStream;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 import sleet.generators.time.TimeDependentSequenceIdGenerator;
 import sleet.utils.zookeeper.ZkMiniCluster;
 
 public class TestSleetIdGenerator {
+  public static final int NUM_INSTANCES = 100;
+
   public static void main(String[] args) {
     ZkMiniCluster cluster = null;
     try {
@@ -21,28 +24,64 @@ public class TestSleetIdGenerator {
       zkconn = "localhost:" + zkconn.substring(zkconn.lastIndexOf(':') + 1);
       System.out.println("ZK connection string: " + zkconn);
 
-      SleetIdGenerator gen = new SleetIdGenerator();
-      Properties props = new Properties();
-      InputStream is = gen.getClass().getResourceAsStream("sleet.properties");
-      props.load(is);
-      is.close();
-      gen.beginIdSession(props);
-      int loops = 1000000;
-      long start = System.currentTimeMillis();
-      long maxseq = (1L << Integer.parseInt(props.getProperty(TimeDependentSequenceIdGenerator.BITS_IN_SEQUENCE_KEY))) - 1L;
-      long highest = -1;
-      for (int i = 0; i < loops; i++) {
-        // System.out.println(SleetIdGenerator.paddedBinary(gen.getId().getId()));
-        long id = gen.getId().getId();
-        long seq = id & maxseq;
-        if (seq > highest) {
-          highest = seq;
-        }
+      Thread.sleep(TimeUnit.SECONDS.toMillis(1));
+      Thread[] threads = new Thread[NUM_INSTANCES];
+      final Object lock = new Object();
+      for (int instance = 0; instance < NUM_INSTANCES; instance++) {
+        Thread t = new Thread() {
+
+          @Override
+          public void run() {
+            try {
+              SleetIdGenerator gen = new SleetIdGenerator();
+              Properties props = new Properties();
+              InputStream is = gen.getClass().getResourceAsStream("sleet.properties");
+              props.load(is);
+              is.close();
+              gen.beginIdSession(props);
+              int loops = 100000;
+              long start = System.currentTimeMillis();
+              long maxseq = (1L << Integer.parseInt(props.getProperty(TimeDependentSequenceIdGenerator.BITS_IN_SEQUENCE_KEY))) - 1L;
+              long highest = -1;
+              for (int i = 0; i < loops; i++) {
+                // System.out.println(SleetIdGenerator.paddedBinary(gen.getId().getId()));
+                long id = gen.getId().getId();
+                long seq = id & maxseq;
+                if (seq > highest) {
+                  highest = seq;
+                }
+              }
+              gen.endIdSession();
+              long duration = System.currentTimeMillis() - start;
+              System.out.println("Duration for " + loops + " ids: " + duration + "ms");
+              System.out.println("Mean ids per ms: " + loops / duration);
+              System.out.println("Max sequence: " + highest);
+              synchronized (lock) {
+                lock.notify();
+              }
+            } catch (Exception e) {
+              e.printStackTrace();
+              return;
+            }
+          }
+
+        };
+        threads[instance] = t;
+        t.start();
       }
-      long duration = System.currentTimeMillis() - start;
-      System.out.println("Duration for " + loops + " ids: " + duration + "ms");
-      System.out.println("Mean ids per ms: " + loops / duration);
-      System.out.println("Max sequence: " + highest);
+      boolean finished = false;
+      while (!finished) {
+        synchronized (lock) {
+          lock.wait(TimeUnit.SECONDS.toMillis(1));
+        }
+        boolean alive = false;
+        for (int instance = 0; instance < threads.length && !alive; instance++) {
+          if (threads[instance].isAlive()) {
+            alive = true;
+          }
+        }
+        finished = !alive;
+      }
     } catch (Exception e) {
       e.printStackTrace();
       return;
